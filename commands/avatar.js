b8170beea84b0ab33c05d778fa1e023e20239332
FikryGanzz <79295079+FikryGanzz@users.noreply.github.com>
2021-03-11 23:05:26 +0700

    Create avatar.js

---
+++ commands/avatar.js
@@ -0,0 +1,13 @@
+const { MessageEmbed } = require("discord.js")
+
+exports.run = async (client, msg, args) => {
+  const input = args.join(' ')
+  const user = client.users.cache.get(input) || client.users.cache.find(x => x.username == input) || msg.mentions.users.first() || msg.author
+  const embed = new MessageEmbed()
+  .setTitle(`${user.tag} Avatar`)
+  .setImage(user.displayAvatarURL({dynamic:true, size:4096}))
+  .setColor("RANDOM")
+  .setFooter(client.user.tag, client.user.displayAvatarURL())
+  .setTimestamp()
+  msg.channel.send(embed)
+}
