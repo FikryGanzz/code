8a210876fcb19a007787f7acc1d65e797faa6d6b
FikryGanzz <79295079+FikryGanzz@users.noreply.github.com>
2021-03-11 23:03:18 +0700

    Create uptime.js

---
+++ commands/uptime.js
@@ -0,0 +1,13 @@
+const ms = require("pretty-ms")
+const { MessageEmbed } = require("discord.js")
+
+exports.run = async(client, message, args) => {
+  const uptime = ms(client.uptime, {verbose: true})
+  const embed = new MessageEmbed()
+  .addField("Bot Uptime", uptime)
+  .setColor("RED")
+  .setFooter(client.user.tag, client.user.displayAvatarURL())
+  .setTimestamp()
+  
+  message.channel.send(embed)
+}
